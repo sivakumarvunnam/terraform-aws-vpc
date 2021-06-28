5f1b55c92320b68f4528faafdbacab5763436dfc
Sivakumar Vunnam <sivakumarvunnam1@gmail.com>
2021-06-28 17:02:21 -0500

    resource to create vpc

---
+++ main.tf
@@ -0,0 +1,18 @@
+#------------------------------------------------------------------------------
+# AWS Virtual Private Network
+#------------------------------------------------------------------------------
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr_block
+  # The CIDR block for the VPC.
+  enable_dns_support = true
+  # A boolean flag to enable/disable DNS support in the VPC.
+  enable_dns_hostnames = true
+  # A boolean flag to enable/disable DNS hostnames in the VPC.
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = "${var.name_prefix}-vpc"
+    },
+  )
+
+}
