3784df63b204adb87574ca4633b57692bfcf055b
Sivakumar Vunnam <sivakumarvunnam1@gmail.com>
2021-06-28 21:30:47 -0500

    resource to create private subnets, route table, route to access internet using NAT and attach private subnets to route table

--- main.tf
+++ main.tf
@@ -106,3 +106,55 @@ resource "aws_route_table_association" "public_internet_route_table_associations
   subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
   route_table_id = element(aws_route_table.public_subnets_route_table.*.id, count.index)
 }
+
+#------------------------------------------------------------------------------
+# AWS Subnets - Private
+#------------------------------------------------------------------------------
+# Subnets
+resource "aws_subnet" "private_subnets" {
+  count                   = length(var.availability_zones)
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = element(var.availability_zones, count.index)
+  cidr_block              = element(var.private_subnets_cidrs_per_availability_zone, count.index)
+  map_public_ip_on_launch = false
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = "${var.name_prefix}-private-net-${element(var.availability_zones, count.index)}"
+    },
+  )
+}
+
+# Private route table
+resource "aws_route_table" "private_subnets_route_table" {
+  count  = length(var.availability_zones)
+  vpc_id = aws_vpc.vpc.id
+  tags = merge(
+    var.additional_tags,
+    {
+      Name = "${var.name_prefix}-private-rt-${element(var.availability_zones, count.index)}"
+    },
+  )
+}
+
+# Private route to access internet
+resource "aws_route" "private_internet_route" {
+  count = length(var.availability_zones)
+  depends_on = [
+    aws_internet_gateway.internet_gw,
+    aws_route_table.private_subnets_route_table,
+  ]
+  route_table_id         = element(aws_route_table.private_subnets_route_table.*.id, count.index)
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = var.single_nat ? aws_nat_gateway.nat_gw.0.id : element(aws_nat_gateway.nat_gw.*.id, count.index)
+}
+
+# Association of Route Table to Subnets
+resource "aws_route_table_association" "private_internet_route_table_associations" {
+  count     = length(var.private_subnets_cidrs_per_availability_zone)
+  subnet_id = element(aws_subnet.private_subnets.*.id, count.index)
+  route_table_id = element(
+    aws_route_table.private_subnets_route_table.*.id,
+    count.index,
+  )
+}
