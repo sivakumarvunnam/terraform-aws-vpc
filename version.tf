06533ada91959f31bcee84b205a676cd232e6294
Sivakumar Vunnam <sivakumarvunnam1@gmail.com>
2021-09-30 12:19:27 -0500

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,5 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_force_path_style         = true
+  access_key                  = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key                  = "${var.AWS_SECRET_ACCESS_KEY}"
 }
 terraform {
   required_version = ">= 0.13"
